63420e195f8cf4f76be65af7b22296179a71ff26
PC-10121024\hoon <imode7@naver.com>
2022-11-16 17:31:26 +0900

    다크모드

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from "react";
+
+export const DarkModeContext = createContext(null);
+
+export function DarkModeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => setDarkMode((mode) => !mode);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
